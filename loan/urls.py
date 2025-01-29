4b53846936e877d3330b4cdf5c367f7608aabfa6
morachake <jacobmoracha@gmail.com>
2025-01-29 23:58:01 +0300

    Add loan statistics endpoint to loan URLs

--- loan/urls.py
+++ loan/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import LoanViewSet
+from .views import LoanViewSet, LoanStatisticsView
+
+app_name = 'loan'
 
 router = DefaultRouter()
-router.register(r'loans', LoanViewSet)
+router.register(r'loans', LoanViewSet, basename='loan')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('statistics/', LoanStatisticsView.as_view(), name='loan-statistics'),
 ]
